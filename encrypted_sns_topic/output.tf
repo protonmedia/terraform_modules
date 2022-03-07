3765c9b9546fcd740d797b49771d8af0eb5e2707
Vinny Osiro <vinicius.osiro@gmail.com>
2022-03-07 17:11:12 +0800

    feat: adding SNS topic for Chatbot

--- encrypted_sns_topic/output.tf
+++ encrypted_sns_topic/output.tf
@@ -4,16 +4,16 @@ output "sns_arn" {
 }
 
 output "key_arn" {
-  value       = aws_kms_key.this.arn
+  value       = module.aws_kms_key.this.arn
   description = "The Amazon Resource Name (ARN) of the key"
 }
 
 output "alias_arn" {
-  value       = aws_kms_alias.this.arn
+  value       = module.aws_kms_alias.this.arn
   description = "The Amazon Resource Name (ARN) of the key alias"
 }
 
 output "alias" {
-  value       = "alias/${var.alias}"
+  value       = module.aws_kms_key.alias
   description = "The key alias"
 }
