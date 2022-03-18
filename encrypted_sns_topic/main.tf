6eacf406ca26223c200f12e7d27beb354c3dfbcc
Vinny Osiro <vinicius.osiro@gmail.com>
2022-03-18 14:54:24 +0800

    chore: (encrypted_sns_topic) updated to use an AWS managed key

--- encrypted_sns_topic/main.tf
+++ encrypted_sns_topic/main.tf
@@ -1,11 +1,4 @@
-module "sns_kms_key" {
-  source      = "../kms_key"
-  description = "Key used to encrypt messages on SNS Topic ${var.name}"
-  alias       = "sns/${var.name}"
-  policy      = jsonencode(data.aws_iam_policy_document.sns.json)
-}
-
 resource "aws_sns_topic" "this" {
   display_name      = var.name
-  kms_master_key_id = module.sns_kms_key.alias
+  kms_master_key_id = "alias/aws/sns"
 }
--- encrypted_sns_topic/output.tf
+++ encrypted_sns_topic/output.tf
@@ -2,18 +2,3 @@ output "sns_arn" {
   value       = aws_sns_topic.this.id
   description = "The ARN of the SNS topic"
 }
-
-output "key_arn" {
-  value       = module.sns_kms_key.key_arn
-  description = "The Amazon Resource Name (ARN) of the key"
-}
-
-output "alias_arn" {
-  value       = module.sns_kms_key.alias_arn
-  description = "The Amazon Resource Name (ARN) of the key alias"
-}
-
-output "alias" {
-  value       = module.sns_kms_key.alias
-  description = "The key alias"
-}
